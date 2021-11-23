abd1a37225061f05e5defd33324a982f1cc54f6a
Marcin Kunicki <masel.popowo@gmail.com>
2021-11-23 23:47:52 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'acalc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'acalc-client';
+}
